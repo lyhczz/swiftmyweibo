73656b9a31cf3fda14be7b4a94828f837f501b3b
mposthh <mposthh@mposthhdemacbook-pro.local>
2015-10-26 19:55:06 +0800

    确定项目目录结构

---
+++ Myweibo/Classes/YHTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  YHTabBarController.swift
+//  Myweibo
+//
+//  Created by mposthh on 15/10/26.
+//  Copyright © 2015年 mposhh. All rights reserved.
+//
+
+import UIKit
+
+class YHTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
